38951dec797726c72296c969bd7e99fe958e5bf0
stawil15@jcu.edu <stawil15@jcu.edu@f2b38f04-7ffb-ff69-726f-1a1052b76ea1>
2014-03-20 18:49:58 +0000

    Modified Action Bar and Navigation Menu

--- to-genda/src/edu/jcu/cs470/togenda/MainActivity.java
+++ to-genda/src/edu/jcu/cs470/togenda/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -89,15 +90,15 @@ public class MainActivity extends Activity
         return super.onCreateOptionsMenu(menu);
     }
 
-    @Override
+	@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
         return super.onOptionsItemSelected(item);
     }
 
