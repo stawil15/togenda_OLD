35310aa7e922242375c7ad16d65b44466a493a6f
stawil15@jcu.edu <stawil15@jcu.edu@f2b38f04-7ffb-ff69-726f-1a1052b76ea1>
2014-04-20 17:49:32 +0000

    Libraries imported. Calendar fragment Settings Intent

--- ToGenda/src/edu/jcu/cs470/togenda/MainActivity.java
+++ ToGenda/src/edu/jcu/cs470/togenda/MainActivity.java
@@ -153,7 +153,7 @@ public class MainActivity extends FragmentActivity {
     public boolean onPrepareOptionsMenu(Menu menu) {
         // If the nav drawer is open, hide action items related to the content view
         boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-        menu.findItem(R.id.new_event).setVisible(!drawerOpen);
+        menu.findItem(R.id.new_event).setVisible(!drawerOpen&&gPosition!=2);
         return super.onPrepareOptionsMenu(menu);
     }
 
