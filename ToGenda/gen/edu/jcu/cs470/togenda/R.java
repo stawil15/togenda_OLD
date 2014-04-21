8c23522363fd5573a37c79c408972a0cf16398ef
stawil15@jcu.edu <stawil15@jcu.edu@f2b38f04-7ffb-ff69-726f-1a1052b76ea1>
2014-04-21 23:30:40 +0000

    DateTimePicker and NineOldAndroids Library added.

--- ToGenda/gen/edu/jcu/cs470/togenda/R.java
+++ ToGenda/gen/edu/jcu/cs470/togenda/R.java
@@ -620,43 +620,49 @@ containing a value of this type.
     public static final class string {
         public static final int action_settings=0x7f07000d;
         public static final int app_name=0x7f070003;
+        public static final int cancel_button=0x7f070014;
         public static final int color_stripe=0x7f07000a;
+        public static final int create_button=0x7f070015;
         public static final int day_name_format=0x7f070000;
         public static final int desc=0x7f070008;
+        public static final int discription_hint=0x7f070017;
         public static final int drawer_close=0x7f070011;
         public static final int drawer_open=0x7f070010;
+        public static final int due_date=0x7f070016;
         public static final int getColor=0x7f07000f;
         public static final int hello_world=0x7f070004;
         public static final int invalid_date=0x7f070001;
         public static final int menu_settings=0x7f070005;
         public static final int month_name_format=0x7f070002;
         public static final int new_event=0x7f07000b;
+        public static final int new_task=0x7f070019;
         public static final int overflow=0x7f070009;
-        public static final int pref_default_display_name=0x7f070019;
-        public static final int pref_description_social_recommendations=0x7f070017;
+        public static final int pref_default_display_name=0x7f07001f;
+        public static final int pref_description_social_recommendations=0x7f07001d;
         /**  Example settings for Data & Sync 
          */
-        public static final int pref_header_data_sync=0x7f07001b;
+        public static final int pref_header_data_sync=0x7f070021;
         /**  Strings related to Settings 
  Example General settings 
          */
-        public static final int pref_header_general=0x7f070015;
+        public static final int pref_header_general=0x7f07001b;
         /**  Example settings for Notifications 
          */
-        public static final int pref_header_notifications=0x7f07001e;
-        public static final int pref_ringtone_silent=0x7f070021;
-        public static final int pref_title_add_friends_to_messages=0x7f07001a;
-        public static final int pref_title_display_name=0x7f070018;
-        public static final int pref_title_new_message_notifications=0x7f07001f;
-        public static final int pref_title_ringtone=0x7f070020;
-        public static final int pref_title_social_recommendations=0x7f070016;
-        public static final int pref_title_sync_frequency=0x7f07001c;
-        public static final int pref_title_system_sync_settings=0x7f07001d;
-        public static final int pref_title_vibrate=0x7f070022;
+        public static final int pref_header_notifications=0x7f070024;
+        public static final int pref_ringtone_silent=0x7f070027;
+        public static final int pref_title_add_friends_to_messages=0x7f070020;
+        public static final int pref_title_display_name=0x7f07001e;
+        public static final int pref_title_new_message_notifications=0x7f070025;
+        public static final int pref_title_ringtone=0x7f070026;
+        public static final int pref_title_social_recommendations=0x7f07001c;
+        public static final int pref_title_sync_frequency=0x7f070022;
+        public static final int pref_title_system_sync_settings=0x7f070023;
+        public static final int pref_title_vibrate=0x7f070028;
+        public static final int task_name_hint=0x7f070018;
         public static final int title=0x7f070007;
         public static final int title_activity_calendar_fragment=0x7f070013;
         public static final int title_activity_main=0x7f070006;
-        public static final int title_activity_settings=0x7f070014;
+        public static final int title_activity_settings=0x7f07001a;
         public static final int title_activity_task=0x7f07000c;
         public static final int title_activity_task_creator=0x7f07000e;
         public static final int title_activity_to_do_fragment=0x7f070012;
